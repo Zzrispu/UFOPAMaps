9fd0d9f00c2d7f65fb62b80e9d9b3611cc5b4f80
Zzrispu <122392767+Zzrispu@users.noreply.github.com>
2024-09-16 11:56:08 -0300

    Models Update

--- models/users.models.js
+++ models/users.models.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
 
 const user = new mongoose.Schema({
-    name: {
+    username: {
         type: String,
         required: [true, 'Adicione o nome do usuário.']
     },
     password: {
         type: String,
         required: [true, 'Adicione a senha do usuário.']
-    }
+    },
+    admin: {
+        type: Boolean,
+        required: [true, 'Espeficique o cargo do usuário.']
+    },
+    points: Number
 })
 
 const User = mongoose.model('Users', user, 'Users');
