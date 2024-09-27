633a6dbcd0bc2edc0bbece787a53005824bbb90e
Zzrispu <122392767+Zzrispu@users.noreply.github.com>
2024-09-26 22:31:59 -0300

    Update dashboard.route.js

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -4,18 +4,18 @@ import User from "../models/users.models.js";
 const router = Router();
 
 router.get('/dashboard', async (request, response) => {
-    // if (!request.session.userId) {
-    //     request.session.error = 3;
-    //     return response.redirect('/login');
-    // }
-    // else {
-    //     const userId = await User.findById(request.session.userId);
-    //     if (!userId || userId.admin == false ) {
-    //         console.log('usuario não autorizado');
-    //         request.session.error = 4;
-    //         return response.redirect('/login');
-    //     };
-    // }
+    if (!request.session.userId) {
+        request.session.error = 3;
+        return response.redirect('/login');
+    }
+    else {
+        const userId = await User.findById(request.session.userId);
+        if (!userId || userId.admin == false ) {
+            console.log('usuario não autorizado');
+            request.session.error = 4;
+            return response.redirect('/login');
+        };
+    }
     response.render('dashboard');
 });
 
